bf5140515887d44ea0bc6a1b4a1ee3fec32d40c8
David Atanda <ololadedavid15@gmail.com>
2023-08-08 13:15:39 +0100

    variable types

---
+++ vartypes.py
@@ -0,0 +1,22 @@
+# List Variable Type
+name_list = ['James', 'Peter', 'Daniel', 'Paul']
+my_list = ['David', 'Micheal']
+name_list[2] = 'David'
+
+print(name_list)
+# print(name_list + my_list)
+
+# Tupple Variable Type
+name_tupple = ('Favour', 'Victor', 'Benjamin', 'Stella')
+my_tupple = ('Festus', 'Felix')
+
+print(name_tupple)
+# print(name_tupple + my_tupple)
+
+# Dictionary Variable Type
+name_dict = {}
+tinydict = {'name': 'john','code':6734, 'dept': 'Software Engineering'}
+name_dict['one'] = 'This is one'
+
+print(name_dict, tinydict.keys())
+# It's impossible to concatenate dict
